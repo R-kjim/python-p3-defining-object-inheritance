74a1bc5803e8ad5c8bfae504518c87d2b48029c2
R-kjim <rkayjim@gmail.com>
2024-09-02 19:00:00 +0300

    objects inheritance

--- lib/car.py
+++ lib/car.py
@@ -2,3 +2,9 @@ from vehicle import Vehicle
 
 class Car(Vehicle):
     pass
+    # super()
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+
+subaru=Car(14,65)
+print(subaru.go())
